45ca2d8c0104e12d6a1501dddd69865a7994d1a0
navid farahzadi <na.farahzadi@gmail.com>
2017-09-22 17:25:56 +0330

    landing screen content added. styes initialized

--- src/components/LandingScreen/LandingScreen.js
+++ src/components/LandingScreen/LandingScreen.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
+import logo from './images/icon.svg'
+import './LandingScreen.css'
+
 
 const LandingScreen = () => (
-  <div>Landing screen</div>
+  <div className="landing-screen">
+    <div>
+      <img src={logo} width="400" alt="دایره" />
+    </div>
+    <p>
+      <span>
+        آن که پرنقش زد این دایره مینایی
+      </span>
+      <span>
+        کس ندانست که در گردش پرگار چه کرد
+      </span>
+    </p>
+  </div>
 )
 
 export { LandingScreen }
