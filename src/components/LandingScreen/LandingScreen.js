b813fe748300d72018db25ad411138b055fb0adc
navid farahzadi <na.farahzadi@gmail.com>
2017-09-22 21:56:15 +0330

    landing page first draft

--- src/components/LandingScreen/LandingScreen.js
+++ src/components/LandingScreen/LandingScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import logo from './images/icon.svg'
+import logo from '../../assets/images/icon.svg'
 import './LandingScreen.css'
 
 
@@ -9,14 +9,19 @@ const LandingScreen = () => (
     <div>
       <img src={logo} width="400" alt="دایره" />
     </div>
-    <p>
-      <span>
-        آن که پرنقش زد این دایره مینایی
+    <blockquote className="poem landing-screen__poem">
+      <span className="poem__verse">
+        آن کــه پـــرنــقــش زد ایــن
+        <b className="text-orange"> دایـــــره </b>
+        میـنـایـی
       </span>
-      <span>
+      <span className="poem__verse">
         کس ندانست که در گردش پرگار چه کرد
       </span>
-    </p>
+    </blockquote>
+    <h1 className="landing-screen__first-header">آژانس تبلیغات دیجیتال دایره</h1>
+    <h2 className="landing-screen__second-header">راه حل های خلاق از ایده تا اجرا</h2>
+    <br />
   </div>
 )
 
