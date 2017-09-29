cc2a9c32d36f231f6f2c5895c8561c67c45eb87c
navid farahzadi <na.farahzadi@gmail.com>
2017-09-29 20:11:47 +0330

    LandingScreen style improved

--- src/components/LandingScreen/LandingScreen.js
+++ src/components/LandingScreen/LandingScreen.js
@@ -6,22 +6,23 @@ import './LandingScreen.css'
 
 const LandingScreen = () => (
   <div className="landing-screen">
-    <div>
-      <img src={logo} width="400" alt="دایره" />
-    </div>
-    <blockquote className="poem landing-screen__poem">
-      <span className="poem__verse">
-        آن کــه پـــرنــقــش زد ایــن
-        <b className="text-orange"> دایـــــره </b>
-        میـنـایـی
-      </span>
-      <span className="poem__verse">
-        کس ندانست که در گردش پرگار چه کرد
-      </span>
-    </blockquote>
-    <h1 className="landing-screen__first-header">آژانس تبلیغات دیجیتال دایره</h1>
-    <h2 className="landing-screen__second-header">راه حل های خلاق از ایده تا اجرا</h2>
-    <br />
+    <section className="landing-screen__content">
+      <div>
+        <img src={logo} width="400" alt="دایره" />
+      </div>
+      <blockquote className="poem landing-screen__poem">
+        <span className="poem__verse">
+          آن کــه پـــرنــقــش زد ایــن
+          <b className="text-orange"> دایـــــره </b>
+          میـنـایـی
+        </span>
+        <span className="poem__verse">
+          کس ندانست که در گردش پرگار چه کرد
+        </span>
+      </blockquote>
+      <h1 className="landing-screen__first-header">آژانس تبلیغات دیجیتال دایره</h1>
+      <h2 className="landing-screen__second-header">راه حل های خلاق از ایده تا اجرا</h2>
+    </section>
   </div>
 )
 
