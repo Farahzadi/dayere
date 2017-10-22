dc9a12281acaa925bf1ad9c457f21c1840bf7da0
navid farahzadi <na.farahzadi@gmail.com>
2017-10-22 22:18:29 +0330

    react transition gropp added

--- src/components/LandingScreen/LandingScreen.js
+++ src/components/LandingScreen/LandingScreen.js
@@ -2,34 +2,63 @@ import React from 'react'
 import {
   Link
 } from 'react-router-dom'
+import {
+  Transition
+} from 'react-transition-group'
 
 import logo from '../../assets/images/icon.svg'
 import './LandingScreen.css'
 
+const defaultStyle = {
+  transition: 'all 1s'
+}
+
+const transitionStyle = {
+  entering: {
+    opacity: 0
+  },
+  entered: {
+    opacity: 1
+  }
+}
+
+const Fade = ({ children, ...props }) => (
+  <Transition timeout={300} appear {...props}>
+    {
+      state => (
+        <div style={{ ...defaultStyle, ...transitionStyle[state] }}>
+          {children}
+        </div>
+      )
+    }
+  </Transition>
+)
 
 const LandingScreen = () => (
-  <div className="landing-screen">
-    <section className="landing-screen__content">
-      <div>
-        <img src={logo} width="350" alt="دایره" />
-      </div>
-      <blockquote className="poem landing-screen__poem">
-        <span className="poem__verse">
-          آن کــه پـــرنــقــش زد ایــن
-          <b className="text-orange"> دایـــــره </b>
-          میـنـایـی
-        </span>
-        <span className="poem__verse">
-          کس ندانست که در گردش پرگار چه کرد
-        </span>
-      </blockquote>
-      <h1 className="landing-screen__first-header">آژانس تبلیغات دیجیتال دایره</h1>
-      <h2 className="landing-screen__second-header">راه حل های خلاق از ایده تا اجرا</h2>
-    </section>
-    <Link to="/what-we-do" className="landing-screen__pager">
-      <span className="icon ion-ios-arrow-down" />
-    </Link>
-  </div>
+  <Fade in>
+    <div className="landing-screen">
+      <section className="landing-screen__content">
+        <div>
+          <img src={logo} width="350" alt="دایره" />
+        </div>
+        <blockquote className="poem landing-screen__poem">
+          <span className="poem__verse">
+            آن کــه پـــرنــقــش زد ایــن
+            <b className="text-orange"> دایـــــره </b>
+            میـنـایـی
+          </span>
+          <span className="poem__verse">
+            کس ندانست که در گردش پرگار چه کرد
+          </span>
+        </blockquote>
+        <h1 className="landing-screen__first-header">آژانس تبلیغات دیجیتال دایره</h1>
+        <h2 className="landing-screen__second-header">راه حل های خلاق از ایده تا اجرا</h2>
+      </section>
+      <Link to="/what-we-do" className="landing-screen__pager">
+        <span className="icon ion-ios-arrow-down" />
+      </Link>
+    </div>
+  </Fade>
 )
 
 export { LandingScreen }
