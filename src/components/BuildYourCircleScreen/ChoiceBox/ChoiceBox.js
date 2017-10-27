a8754bed75064d92696f978a1e3897c7de07fc34
navid farahzadi <na.farahzadi@gmail.com>
2017-10-27 16:07:35 +0330

    click handled

--- src/components/BuildYourCircleScreen/ChoiceBox/ChoiceBox.js
+++ src/components/BuildYourCircleScreen/ChoiceBox/ChoiceBox.js
@@ -1,21 +1,41 @@
 import React from 'react'
 
 
-const ChoiceBox = ({ image, title, text }) => (
-  <a href="" className="choice-box">
-    <div className="choice-box__header">
-      <img src={image} alt={title} />
-      <h2 className="choice-box__title">{title}</h2>
-      <div className="pretty-checkbox__container">
-        <input type="checkbox" name="service" />
-        <span className="ion-android-checkmark-circle pretty-checkbox" />
-      </div>
-    </div>
-    <div className="choice-box__body">
-      {text}
-    </div>
-  </a>
-)
+class ChoiceBox extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: false
+    }
+  }
+
+  render() {
+    const { image, title, text } = this.props
+    const { selected } = this.state
+    return (
+      <a
+        href=""
+        className="choice-box"
+        onClick={(e) => {
+          e.preventDefault()
+          this.setState({ selected: !selected })
+        }}
+      >
+        <div className="choice-box__header">
+          <img src={image} alt={title} />
+          <h2 className="choice-box__title">{title}</h2>
+          <div className="pretty-checkbox__container">
+            <input type="checkbox" name="service" checked={selected} />
+            <span className="ion-android-checkmark-circle pretty-checkbox" />
+          </div>
+        </div>
+        <div className="choice-box__body">
+          {text}
+        </div>
+      </a>
+    )
+  }
+}
 
 
 export { ChoiceBox }
