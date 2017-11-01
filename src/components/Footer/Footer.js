284fbaa72a58ce63f4d7a7f7847d3149502aeb60
navid farahzadi <na.farahzadi@gmail.com>
2017-11-01 10:38:51 +0330

    social media links added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,6 +3,10 @@ import React from 'react'
 import './Footer.css'
 import telephone from '../../assets/images/old-typical-phone.svg'
 import email from '../../assets/images/at.svg'
+import telegram from '../../assets/images/social-media/telegram-logo.svg'
+import instagram from '../../assets/images/social-media/instagram-logo.svg'
+import linkedin from '../../assets/images/social-media/linkedin-logo.svg'
+import twitter from '../../assets/images/social-media/twitter-logo.svg'
 
 
 const Footer = () => (
@@ -18,6 +22,20 @@ const Footer = () => (
         <img className="contact-section__icon" src={telephone} alt="telephone" />
         <a href="tel:02122069682" className="contact-section__link">
           02122069682
+        </a>
+      </div>
+      <div className="contact-section">
+        <a className="social-icon" href="">
+          <img height="32" src={telegram} alt="telegram" />
+        </a>
+        <a className="social-icon" href="">
+          <img height="32" src={instagram} alt="instagram" />
+        </a>
+        <a className="social-icon" href="">
+          <img height="32" src={linkedin} alt="linkedin" />
+        </a>
+        <a className="social-icon" href="">
+          <img height="32" src={twitter} alt="twitter" />
         </a>
       </div>
     </div>
